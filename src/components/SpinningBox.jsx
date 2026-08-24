ce26a47a67f19ea96ed56a99fb34c34ae2f90796
Eric Chung <rchung.eric@gmail.com>
2026-08-25 00:03:55 +1000

    Add spinning box controls

--- src/components/SpinningBox.jsx
+++ src/components/SpinningBox.jsx
@@ -1,8 +1,19 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { folder, useControls } from "leva";
 
 export function SpinningBox() {
   const meshRef = useRef(null);
+
+  const { size, color } = useControls({
+    "Spinning Box": folder(
+      {
+        size: { value: 2, min: 0.1, max: 10, step: 0.1 },
+        color: "#ffff00",
+      },
+      { collapsed: true },
+    ),
+  });
 
   useFrame((_, delta) => {
     if (!meshRef.current) return;
@@ -12,8 +23,8 @@ export function SpinningBox() {
 
   return (
     <mesh ref={meshRef} position={[0, 0, -15]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="yellow" />
+      <boxGeometry args={[size, size, size]} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 }
